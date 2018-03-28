edcf7a98153defe2b391b0dde6b9b70be071b99e
Patricia Nykaza <pnykaza@calstatela.edu>
2018-03-28 22:12:49 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,8 +3,15 @@ class Dog
     @name = dog_name
   end
 
-  def name=
-    
+  def name=(dog_name)
+    @name
   end
   
+  def breed(dog_breed)
+    @dog_breed = dog_breed
+  end
+
+  def breed=(dog_breed)
+    @dog_breed
+  end
 end
